137fea2b60603c57ff612630f3aab3cab0eaf0bb
Jacob <jacob.goodridge@hotmail.com>
2020-04-13 18:01:16 +1000

    Update OneDrive.sh

--- OneDrive.sh
+++ OneDrive.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-[ -d /usr/local/etc/OneDrive ] && mv -f /usr/local/etc/OneDrive /usr/local/etc/OneDrive.bak
+[ -d /usr/local/etc/OneDrive ] && sudo mv -f /usr/local/etc/OneDrive /usr/local/etc/OneDrive.bak
 sudo mkdir -p /usr/local/etc/OneDrive
 cd /usr/local/etc/OneDrive
 sudo wget --no-check-certificate -q -O json-parser "https://raw.githubusercontent.com/0oVicero0/OneDrive/master/Business/json-parser"
@@ -9,7 +9,7 @@ sudo wget --no-check-certificate -q -O onedrive-d "https://raw.githubusercontent
 sudo wget --no-check-certificate -q -O onedrive-authorize "https://raw.githubusercontent.com/0oVicero0/OneDrive/master/Business/onedrive-authorize"
 sudo wget --no-check-certificate -q -O onedrive-base "https://raw.githubusercontent.com/0oVicero0/OneDrive/master/Business/onedrive-base"
 sudo wget --no-check-certificate -q -O onedrive.cfg "https://raw.githubusercontent.com/0oVicero0/OneDrive/master/Business/onedrive.cfg"
-chmod -R a+x /usr/local/etc/OneDrive
+sudo chmod -R a+x /usr/local/etc/OneDrive
 sudo ln -sf /usr/local/etc/OneDrive/onedrive /usr/local/bin/
 sudo ln -sf /usr/local/etc/OneDrive/onedrive-d /usr/local/bin/
 [ -d /usr/local/etc/OneDrive.bak ] && {

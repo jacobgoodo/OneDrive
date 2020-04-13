138a1a920209b4d830c1c25e0cc37d4467d877f9
Jacob <jacob.goodridge@hotmail.com>
2020-04-13 18:05:58 +1000

    Update OneDrive.sh

--- OneDrive.sh
+++ OneDrive.sh
@@ -3,12 +3,12 @@
 [ -d /usr/local/etc/OneDrive ] && sudo mv -f /usr/local/etc/OneDrive /usr/local/etc/OneDrive.bak
 sudo mkdir -p /usr/local/etc/OneDrive
 cd /usr/local/etc/OneDrive
-sudo wget --no-check-certificate -q -O json-parser "https://raw.githubusercontent.com/0oVicero0/OneDrive/master/Business/json-parser"
-sudo wget --no-check-certificate -q -O onedrive "https://raw.githubusercontent.com/0oVicero0/OneDrive/master/Business/onedrive"
-sudo wget --no-check-certificate -q -O onedrive-d "https://raw.githubusercontent.com/0oVicero0/OneDrive/master/Business/onedrive-d"
-sudo wget --no-check-certificate -q -O onedrive-authorize "https://raw.githubusercontent.com/0oVicero0/OneDrive/master/Business/onedrive-authorize"
-sudo wget --no-check-certificate -q -O onedrive-base "https://raw.githubusercontent.com/0oVicero0/OneDrive/master/Business/onedrive-base"
-sudo wget --no-check-certificate -q -O onedrive.cfg "https://raw.githubusercontent.com/0oVicero0/OneDrive/master/Business/onedrive.cfg"
+sudo wget --no-check-certificate -q -O json-parser "https://raw.githubusercontent.com/jacobgoodo/OneDrive/master/Business/json-parser"
+sudo wget --no-check-certificate -q -O onedrive "https://raw.githubusercontent.com/jacobgoodo/OneDrive/master/Business/onedrive"
+sudo wget --no-check-certificate -q -O onedrive-d "https://raw.githubusercontent.com/jacobgoodo/OneDrive/master/Business/onedrive-d"
+sudo wget --no-check-certificate -q -O onedrive-authorize "https://raw.githubusercontent.com/jacobgoodo/OneDrive/master/Business/onedrive-authorize"
+sudo wget --no-check-certificate -q -O onedrive-base "https://raw.githubusercontent.com/jacobgoodo/OneDrive/master/Business/onedrive-base"
+sudo wget --no-check-certificate -q -O onedrive.cfg "https://raw.githubusercontent.com/jacobgoodo/OneDrive/master/Business/onedrive.cfg"
 sudo chmod -R a+x /usr/local/etc/OneDrive
 sudo ln -sf /usr/local/etc/OneDrive/onedrive /usr/local/bin/
 sudo ln -sf /usr/local/etc/OneDrive/onedrive-d /usr/local/bin/
